e5f234f270abb6c0e914b1231cb60356312dad13
omazing-dev <dev@omazing.dev>
2025-08-13 11:28:02 -0500

    Add folder Services

---
+++ src/backend/PropertyApp.Application/Interfaces/IPropertyRepository.cs
@@ -0,0 +1,20 @@
+﻿using PropertyApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyApp.Application.Interfaces
+{
+    public interface IPropertyRepository
+    {
+        Task<(IEnumerable<PropertyE> Properties, int Total)> GetFilteredAsync(
+            string? name,
+            string? address,
+            decimal? minPrice,
+            decimal? maxPrice,
+            int page,
+            int pageSize);
+    }
+}
---
+++ src/backend/PropertyApp.Infraestructure/Repositories/PropertyRepository.cs
@@ -0,0 +1,48 @@
+﻿using MongoDB.Driver;
+using PropertyApp.Application.Interfaces;
+using PropertyApp.Domain.Entities;
+using PropertyApp.Infraestructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyApp.Infraestructure.Repositories
+{
+    public class PropertyRepository : IPropertyRepository
+    {
+        private readonly IMongoCollection<PropertyE> _collection;
+        public PropertyRepository(MongoDbContext context)
+        {
+            _collection = context.Properties;
+        }
+        public async Task<(IEnumerable<PropertyE> Properties, int Total)> GetFilteredAsync(string? name, string? address, decimal? minPrice, decimal? maxPrice, int page, int pageSize)
+        {
+            var filterBuilder = Builders<PropertyE>.Filter;
+            var filter = filterBuilder.Empty;
+
+            if (!string.IsNullOrEmpty(name))
+                filter &= filterBuilder.Regex(p => p.Name, new MongoDB.Bson.BsonRegularExpression(name, "i"));
+
+            if (!string.IsNullOrEmpty(address))
+                filter &= filterBuilder.Regex(p => p.Address, new MongoDB.Bson.BsonRegularExpression(address, "i"));
+
+            if (minPrice.HasValue)
+                filter &= filterBuilder.Gte(p => p.Price, minPrice.Value);
+
+            if (maxPrice.HasValue)
+                filter &= filterBuilder.Lte(p => p.Price, maxPrice.Value);
+
+            var total = (int)await _collection.CountDocumentsAsync(filter);
+
+            var properties = await _collection
+                .Find(filter)
+                .Skip((page - 1) * pageSize)
+                .Limit(pageSize)
+                .ToListAsync();
+
+            return (properties, total);
+        }
+    }
+}
